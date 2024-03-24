b25f48cf3d16b882b1315cfee4bdaf1c0f939182
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2024-03-24 11:50:39 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop and remove the containers
-#docker stop backend 2>/dev/null
-#docker stop frontend 2>/dev/null
-#docker rm -f backend 2>/dev/null
-#docker rm -f frontend 2>/dev/null
+docker stop backend 
+docker stop frontend 
+docker rm -f backend
+docker rm -f frontend
