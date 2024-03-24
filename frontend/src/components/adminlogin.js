4dbe3ebebfeba9d4b502688867880e8296a5db50
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2024-03-24 16:27:38 +0530

    Update adminlogin.js

--- frontend/src/components/adminlogin.js
+++ frontend/src/components/adminlogin.js
@@ -16,7 +16,7 @@ const AdminLogin = ({ history }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://54.173.162.200:5000/admin', {
+      const response = await axios.post('http://18.214.89.116:5000/admin', {
         username,
         password,
       });
