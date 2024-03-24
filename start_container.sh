e418d5707c8231729bd86b99dd842ea1ba891be8
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2024-03-24 10:43:43 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Pull the latest version of the Docker image
+docker pull bhaskaranil/frontend:latest
+docker pull bhaskaranil/backend:latest
+
 # Start the containers
-docker run -d -p 5000:5000 bhaskaranil/backend:latest
-docker run -d -p 3000:3000 bhaskaranil/frontend:latest
+docker run -d -p 5000:5000 --name backend bhaskaranil/backend
+docker run -d -p 3000:3000 --name frontend bhaskaranil/frontend
